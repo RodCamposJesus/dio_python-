81d0257f55c2b226408b8cb4fc89d042feac90b0
Rodrigo <chocoliris@msn.com>
2024-09-01 11:02:57 -0300

    testes dicionarios

---
+++ 01 Estrutura de dados/04 - Dicionarios/02_dicionarios_aninhados.py
@@ -0,0 +1,9 @@
+contatos = {
+    "guilherme@gmail.com": {"Nome":"Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"Nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"Nome": "Chappie","telefone": "3334-9871"},
+    "melaine@gmail.com":{"Nome": "Melaine", "telefone": "3333-7766"}
+}
+
+telefone = contatos["giovanna@gmail.com"]["telefone"]
+print(telefone)
