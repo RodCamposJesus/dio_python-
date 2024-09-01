2b9a34c2d010669550210a87390a69b5fd14870b
Rodrigo <chocoliris@msn.com>
2024-09-01 11:22:36 -0300

    testes declarando dicionários

---
+++ 01 Estrutura de dados/04 - Dicionarios/00_declarando_dicionarios.py
@@ -0,0 +1,7 @@
+pessoa = {"nome": "Guilherme", "idade": 28}
+print(pessoa)
+
+pessoa = dict(nome="Guilherme", idades= 28)
+
+pessoa["telefone"] = "3333-1234" #{'nome': 'Guilherme', 'idades': 28, 'telefone': '3333-1234'}
+print(pessoa)
