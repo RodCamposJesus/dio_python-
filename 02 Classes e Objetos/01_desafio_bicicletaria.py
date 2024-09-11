faeae0acb76f3a147fcd4b2aff4be39ef5659ae1
Rodrigo <chocoliris@msn.com>
2024-09-11 16:46:39 -0300

    atualizando outro desafio

--- 02 Classes e Objetos/01_desafio_bicicletaria.py
+++ 02 Classes e Objetos/01_desafio_bicicletaria.py
@@ -4,6 +4,7 @@ class Bicicleta:
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
+        
 
     def buzinar(self):
         print("Plin, plin... ")
@@ -15,7 +16,22 @@ class Bicicleta:
     def correr(self):
         print("Vrummmmmm!")
 
-        bike1 = Bicicleta("vermelha", "caloi", 2022, 600)
-        bike1.buzinar()
-        bike1.correr()
-        bike1.parar()
+    #def get_cor(self):
+        #return self.cor
+    
+    #def __str__(self):
+        #return f"Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}"
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {", ".join([f"{chave}={valor}"for chave, valor in self.__dict__.items()])}"
+    
+
+bike1 = Bicicleta("vermelha", "caloi", 2022, 600)
+bike1.buzinar()
+bike1.correr()
+bike1.parar() 
+print(bike1.cor, bike1.modelo, bike1.ano, bike1.valor)   
+
+bike2 = Bicicleta("verde", "monark", 2000, 189)
+print(bike2)
+bike2.correr() #é a mesa coisa se eu fizer: Bicicleta.buzinar(bike2)
