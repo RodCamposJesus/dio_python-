565459d680e617a434f8d26261e5421546535591
Rodrigo <chocoliris@msn.com>
2024-09-12 15:54:28 -0300

    herança simples

---
+++ 04 Herança/01_heranca_simples.py
@@ -0,0 +1,37 @@
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print("Ligando motor")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {", ".join
+    ([f"{chave}={valor}"for chave, valor in self.__dict__.items()])}"
+
+
+class Motocicleta(Veiculo):
+    pass
+
+class Carro(Veiculo):
+    pass
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas) #super serve para invocar um método da classe pai
+        self.carregado = carregado
+
+        
+    
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+
+moto = Motocicleta("preta", "ABC-1234",2)
+carro = Carro("branco", "XBE-0098", 4)
+caminhao = Caminhao("roxo", "GFD-8712", 8, True)
+
+print(moto)
+print(carro)
+print(caminhao)
