14878a6b3c7e4962e673cc6bb0c41bf77b5db0ba
Rodrigo <chocoliris@msn.com>
2024-09-13 12:39:11 -0300

    heranças multiplas

--- 04 Herança/02_heranca_multipla.py
+++ 04 Herança/02_heranca_multipla.py
@@ -13,24 +13,31 @@ class Mamifero(Animal):
         self.cor_pelo = cor_pelo
         super().__init__(**kw)
 
+   
+
 class Ave(Animal):
     def __init__(self,cor_bico, **kw):
         self.cor_bico = cor_bico
-        super().__init__(**kw)# key args utilizado para utilizar os parâmetros da classe mãe
-
-    
+        super().__init__(**kw)# key args utilizado para utilizar os 
+        #argumentos(parâmetros) da classe pai(dexamos nos 
+        # nas classes "filhas" somente os #
+        # args correspondes dessas classes.S)
 
 class Gato(Mamifero):
     pass
 
 
 class Ornitorrinco(Mamifero, Ave):
-    pass
+    def __init__(self,cor_bico, cor_pelo, nro_patas):
+        #print(Ornitorrinco.__mro__)#vai trazer a ordem de resolução
+        #print(Ornitorrinco.mro()) #outr maneira de trazer a oerdem de resolução
+        
+        super().__init__(cor_pelo = cor_pelo, cor_bico = cor_bico, 
+    nro_patas = nro_patas)
+    
 
-
-
-#gato = Gato(4, "preto")
-#print(gato)
+gato = Gato(nro_patas= 4, cor_pelo="preto", )
+print(gato)
 
 ornitorrinco = Ornitorrinco(nro_patas=2,cor_pelo="vermelho", cor_bico="laranja")
 print(ornitorrinco)
