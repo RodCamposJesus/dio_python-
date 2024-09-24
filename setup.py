6a24f2982deaeb6b8f9ded4df167d5deff0d8120
Rodrigo <chocoliris@msn.com>
2024-09-24 13:28:07 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='surf_simulation',  # Nome do seu pacote
+    version='0.1',           # Versão inicial
+    packages=find_packages(), # Encontra automaticamente os pacotes no projeto
+)
