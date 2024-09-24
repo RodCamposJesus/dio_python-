bae395751fb34a8037cdede776779b6683dc0fff
Rodrigo <chocoliris@msn.com>
2024-09-24 12:41:07 -0300

    corrigindo

--- surf_simulation/condicoes.py
+++ surf_simulation/condicoes.py
@@ -1,6 +1,6 @@
 class Condicoes:
- def __init__(self, tamanh_onda):
-  self.tamanho_onda = tamanh_onda
+ def __init__(self, tamanho_onda):
+  self.tamanho_onda = tamanho_onda
 
   def __str__(self):
    return f"Ondas de {self.tamanho_onda} pés."
--- surf_simulation/simulacao.py
+++ surf_simulation/simulacao.py
@@ -1,2 +1,25 @@
-from SURF SIMULATION.prancha import Prancha
-from SURF SIMULATION.condicoes import Condicoes
+import prancha
+import condicoes
+
+def escolher_prancha(condicoes):
+    pranchas = ["5'8",
+                "5'11",
+                "6'2"
+    ]
+
+    if condicoes.tamanho_onda < 3:
+        return pranchas[0] # Ondas pequenas: prancha menor
+     
+    elif condicoes.tamanho_onda < 5:
+        return pranchas[1]  # Ondas médias: prancha intermediária
+    
+    else:
+        return pranchas[2] # Ondas grandes: prancha maior
+    
+if __name__ == "__main__":
+    tamanho_onda = input("Digite o tamanho das ondas (em pés): ")
+    condicoes_atuais = condicoes.Condicoes(tamanho_onda)
+    prancha_escolhida = escolher_prancha(condicoes_atuais)
+
+print(f"\nCondições: Ondas de {condicoes_atuais.tamanho_onda} pés") 
+print(f"Prancha escolhida: {prancha_escolhida}")
